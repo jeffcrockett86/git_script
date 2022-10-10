65537351c8c03a8c7321110ddce5234dfd1918bb
Gary Crockett <gbcrockett@gmail.com>
2022-10-10 07:03:04 -0400

    changed letter to {char}

---
+++ script.py
@@ -0,0 +1,14 @@
+import random
+
+chars = 'a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 0 1 2 3 4 5 6 7 8 9'
+
+chars = chars.split()
+char = random.choice(chars)
+print(chars)
+
+open('script.sh', 'w').write('''
+sed -i s/e/{char}/ text.txt
+git add .
+git commit -m 'changed letter to {char}'
+git push
+''')
---
+++ script.sh
@@ -0,0 +1,5 @@
+
+sed -i s/e/{char}/ text.txt
+git add .
+git commit -m 'changed letter to {char}'
+git push

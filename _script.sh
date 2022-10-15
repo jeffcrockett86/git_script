aa0931ec11e09d06c18889e33ef7b348e2be3e2f
Gary Crockett <gbcrockett@gmail.com>
2022-10-15 11:53:52 -0400

    replaced N with 2

--- _script.sh
+++ _script.sh
@@ -1,8 +1,50 @@
 
-for num in $(cat numbers.txt) 
-do
-sed -i .bak "s/D/x/" test.txt
+sed -i .bak "s/N/2/" test.txt
 git add .
-git commit -m 'updated test.txt and test.txt.bak'
+git commit -m 'replaced N with 2'
 git push
-done
+
+sed -i .bak "s/L/z/" test.txt
+git add .
+git commit -m 'replaced L with z'
+git push
+
+sed -i .bak "s/G/4/" test.txt
+git add .
+git commit -m 'replaced G with 4'
+git push
+
+sed -i .bak "s/j/D/" test.txt
+git add .
+git commit -m 'replaced j with D'
+git push
+
+sed -i .bak "s/L/Q/" test.txt
+git add .
+git commit -m 'replaced L with Q'
+git push
+
+sed -i .bak "s/C/h/" test.txt
+git add .
+git commit -m 'replaced C with h'
+git push
+
+sed -i .bak "s/S/K/" test.txt
+git add .
+git commit -m 'replaced S with K'
+git push
+
+sed -i .bak "s/W/E/" test.txt
+git add .
+git commit -m 'replaced W with E'
+git push
+
+sed -i .bak "s/y/R/" test.txt
+git add .
+git commit -m 'replaced y with R'
+git push
+
+sed -i .bak "s/6/I/" test.txt
+git add .
+git commit -m 'replaced 6 with I'
+git push
--- script.py
+++ script.py
@@ -3,16 +3,15 @@ import random
 chars = 'a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 0 1 2 3 4 5 6 7 8 9'
 
 chars = chars.split()
-# char_1 = random.choice(chars)
-# char_2 = random.choice(chars)
 # print(chars)
+f = open('_script.sh', 'w')
+for i in range(10):
+    char_1 = random.choice(chars)
+    char_2 = random.choice(chars)
 
-open('_script.sh', 'w').write(f'''
-for num in $(cat numbers.txt) 
-do
-sed -i .bak "s/{random.choice(chars)}/{random.choice(chars)}/" test.txt
+    f.write(f'''
+sed -i .bak "s/{char_1}/{char_2}/" test.txt
 git add .
-git commit -m 'updated test.txt and test.txt.bak'
+git commit -m 'replaced {char_1} with {char_2}'
 git push
-done
 ''')
